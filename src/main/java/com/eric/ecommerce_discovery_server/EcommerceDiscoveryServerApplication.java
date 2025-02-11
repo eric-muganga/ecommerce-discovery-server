d0ada685225aa4c67b0a86f1a8317c6fcb4520e5
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-02-11 18:35:14 +0100

    first commit

---
+++ src/main/java/com/eric/ecommerce_discovery_server/EcommerceDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.eric.ecommerce_discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceDiscoveryServerApplication.class, args);
+	}
+
+}
