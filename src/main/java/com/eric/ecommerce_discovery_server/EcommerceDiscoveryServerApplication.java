9bfa6a752b554bc978843558d8fbf1fe480e7808
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-02-17 03:36:24 +0100

    Feat(EurekaServer): Integrate `@EnableEurekaServer` to activate Eureka Server

--- src/main/java/com/eric/ecommerce_discovery_server/EcommerceDiscoveryServerApplication.java
+++ src/main/java/com/eric/ecommerce_discovery_server/EcommerceDiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package com.eric.ecommerce_discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EcommerceDiscoveryServerApplication {
 
 	public static void main(String[] args) {
